c5325b9ec1b17b1d5ae68c992680f26fef770e63
unknown <SenaManana@ARMAPRCCIFOD646.FormacionSena.RED>
2024-06-13 07:42:57 -0500

    tipado primitivo

---
+++ src/variables-types.ts
@@ -0,0 +1,40 @@
+
+let bookTitle: string = "TypeScript Basics";
+const libraryName: string = "My Library";
+let numberOfBooks: number = 25; 
+let isLibraryOpen: boolean = true;
+
+
+let titulo: string; 
+let autor: string; 
+let genero: string; 
+let idioma: string; 
+let precio: number;
+let formato: string; 
+let isbn: string; 
+let descripcion: string; 
+let estado: string; 
+let ubicacion: string; 
+let fecha_publicacion: Date; 
+let editorial: string; 
+let paginas: number; 
+let dimensiones: string; 
+let peso: string;
+let disponible: boolean; 
+
+titulo = "El Principito";
+autor = "Antoine de Saint-Exupéry";
+genero = "Novela corta, cuento filosófico";
+idioma = "Español";
+precio = 29.99 ;
+formato = "Físico";
+isbn = "9789580009948";
+descripcion = "Un piloto se encuentra varado en el desierto del Sahara después de que su avión sufriera una avería, pero ahí conoce a un pequeño príncipe proveniente de otro planeta.",
+estado = "Nuevo";
+ubicacion = "Colombia";
+fecha_publicacion = new Date("2000-06-01");
+editorial = "Torre de papel";
+paginas = 320;
+dimensiones = 22+ "x"+ 14 +"cm";
+peso = 500+"g"; 
+disponible = true
